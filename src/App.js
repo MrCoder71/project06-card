5743fe4fa64c8d534f48a55ab2d2a76decea5111
Mrcoder <amirsohail4151@gmail.com>
2024-06-28 23:52:59 -0700

    Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div class="cards">
+      <div class="card red">
+        <p class="tip">Subscribe</p>
+        <p class="second-text">@mrcoder71</p>
+      </div>
+      <div class="card blue">
+        <p class="tip">Mr Coder</p>
+        <p class="second-text">@mrcoder71</p>
+      </div>
+      <div class="card green">
+        <p class="tip">Channel</p>
+        <p class="second-text">@mrcoder71</p>
+      </div>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
